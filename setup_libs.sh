3235dcc0738e8070a41fbb093e74485c2faa09d6
Anush Mohan <amohan@pa-magic-061.magicleap.ds>
2015-04-21 21:28:15 -0700

    add comments to resume.html. Refactor setup scripts

--- setup_libs.sh
+++ setup_libs.sh
@@ -7,22 +7,17 @@ sudo pip install -U pdfkit
 
 os_type=$(uname -s)
 echo 'Installing wkhtmltopdf...'
-if [[ ${os_type} == "Darwin" ]]; then
-  if [[ -x $(command -v  wkhtmltopdf) ]]; then
-    echo 'wkhtmltopdf already exists. skipping installation'
-  else
+if [[ -x $(command -v  wkhtmltopdf) ]]; then
+  echo 'wkhtmltopdf already exists. skipping installation'
+else
+  if [[ ${os_type} == "Darwin" ]]; then
     echo 'Downloading wkhtmltopdf...'
     wget http://downloads.sourceforge.net/wkhtmltopdf/wkhtmltox-0.12.2.1_osx-cocoa-x86-64.pkg
     echo 'Installing...'
     sudo installer -pkg ./wkhtmltox-0.12.2.1_osx-cocoa-x86-64.pkg -target /
     echo 'rm wkhtmltox...'
     rm wkhtmltox-0.12.2.1_osx-cocoa-x86-64.pkg
-  fi
-elif [[ ${os_type} = "Linux" ]]; then
-  # See http://stackoverflow.com/a/22592801
-  if [[ -x $(command -v  wkhtmltopdf) ]]; then
-    echo 'wkhtmltopdf already exists. skipping installation'
-  else
+  elif [[ ${os_type} = "Linux" ]]; then
     echo 'downloading wkhtmltopdf...'
     wget http://downloads.sourceforge.net/wkhtmltopdf/wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
     'Installing...'
