395a7a4bc32ba0cd4e4d8194f0aaeca446e15661
Anush Mohan <amohan@magicleap.com>
2018-06-03 18:39:26 -0700

    Updates to experience.
    Updated work experience to include more details about leadership
    positions. Cleaned up unrequired text and removed the Publications and
    Online sections of the resume.
    
    Also updated the setup script.

--- setup_libs.sh
+++ setup_libs.sh
@@ -11,20 +11,9 @@ if [[ -x $(command -v  wkhtmltopdf) ]]; then
   echo 'wkhtmltopdf already exists. skipping installation'
 else
   if [[ ${os_type} == "Darwin" ]]; then
-    echo 'Downloading wkhtmltopdf...'
-    wget http://downloads.sourceforge.net/wkhtmltopdf/wkhtmltox-0.12.2.1_osx-cocoa-x86-64.pkg
-    echo 'Installing...'
-    sudo installer -pkg ./wkhtmltox-0.12.2.1_osx-cocoa-x86-64.pkg -target /
-    echo 'rm wkhtmltox...'
-    rm wkhtmltox-0.12.2.1_osx-cocoa-x86-64.pkg
+    brew install Caskroom/cask/wkhtmltopdf
   elif [[ ${os_type} = "Linux" ]]; then
-    echo 'downloading wkhtmltopdf...'
-    wget http://downloads.sourceforge.net/wkhtmltopdf/wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
-    'Installing...'
-    sudo dpkg -i wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
-    sudo apt-get install -f
-    echo 'rm wkhtmltox...'
-    rm wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
+    sudo apt-get install wkhtmltopdf
   fi
 fi
 
